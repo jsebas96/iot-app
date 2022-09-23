432dd03f9e2f29cce2e210f1faf9f9d9bd929935
Schlvf <axlzbot@gmail.com>
2022-09-22 19:29:10 -0500

    Rebase

---
+++ src/test/java/co/edu/eafit/IoT/IoTApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.IoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
